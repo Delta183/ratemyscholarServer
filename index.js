3467158b0a2849c22304bbf01aac9845403a9b4e
Daniel Figueroa <danfigueroa81@gmail.com>
2021-04-28 00:15:44 -0400

    fix up cor

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const db = require("./db");
 // App Setup
 const app = express();
 const router = new Router();
-app.use(express.json());
-app.use(router);
-app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Express server running on port: ${PORT}`);
